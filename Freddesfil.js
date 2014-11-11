388e6a09a1bb4d84b4bc12ccec0d05ef12f2f953
PhantomBomb <fredrik.voigt@gmail.com>
2014-11-11 16:12:58 +0100

    sak
    sten, sax, påse, med extra, som inte riktigt funkar... kolla gärna
    egenom

---
+++ Freddesfil.js
@@ -0,0 +1,52 @@
+
+if (uC === "r" || uC === "rock") {
+	uC === "rock" }
+else if (uC === "p") {
+	uC === "paper" }
+else if (uC === "s") {
+	uC === "scissors" }
+var cC = Math.random()
+if (cC <= 0.33) {
+	cC === "rock" }
+else if (cC <= 0.66) {
+	cC === "paper" }
+else {cC === "scissors"};
+var comp = function(c1, c2) {
+	if (c1 === c2) {
+		confirm("The result is a tie") }
+	else if (c1 === "rock") {
+		if (c2 === "paper") {
+			confirm("Computer: paper. You lose!") }
+		else {
+			confirm("Computer: scissors. You win!") }}
+	else if (c1 === "paper") {
+		if (c2 === "rock") {
+			confirm("Computer: rock. You win!") }
+		else {
+			confirm("Computer: scissors. You lose!") }}
+	else if (c1 === "scissors") { 
+		if (c2 === "rock") {
+			confirm("Computer: rock. You lose!") }
+		else {
+			confirm("Computer: paper. You win!") }}
+	else if (c1 === "duck") {
+		c1 === "duck" }
+	else {
+		confirm("invalid choice")}
+	};
+comp(uC, cC);
+if (uC === "duck") {
+	var eC = Math.random()
+		if (eC <= 0.2) {
+			confirm("Not a valid choice, but you win") }
+		else if (eC <= 0.4) {
+			confirm("11001 1111 10101  10111 1001 1110!")
+		else if (eC <= 0.6) {
+			confirm("You win, just... just you win.") }
+		else if (eC <= 0.8) {
+			confirm("Today is your lucky day; You win!") }
+		else if (eC <= 0.9999) {
+			confirm("Not really a valid choice, but wathever, you win") }
+		}
+	}
+//*Den funkar inte riktigt...*//
